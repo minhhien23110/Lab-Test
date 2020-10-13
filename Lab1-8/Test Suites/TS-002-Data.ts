9c5610c1553683c4b5854b790bd36482c25e3709
minhhien <minhhien23110@gmail.com>
2020-10-13 14:50:31 +0700

    first commit

---
+++ Lab1-8/Test Suites/TS-002-Data.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-002-Data</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>54849200-c7c7-4106-a215-9e9586683567</testSuiteGuid>
+   <testCaseLink>
+      <guid>881787d5-8aad-4d61-b4b4-525e8bf712cd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-006-Test-Data</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>0d2a57db-cec7-4659-a8b9-375603994fea</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Test_Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>0d2a57db-cec7-4659-a8b9-375603994fea</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>00237e50-8960-428c-9cdb-0fe31625f14a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
